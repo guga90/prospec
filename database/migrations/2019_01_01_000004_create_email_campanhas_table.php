b6f755a7cded4a425d4eefc4a210410a464b8ee6
Gustavo Mendanha <gustavomendanha90@gmail.com>
2019-01-02 18:37:45 -0200

    Add Projeto

---
+++ database/migrations/2019_01_01_000004_create_email_campanhas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmailCampanhasTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        
+        Schema::create('email_campanhas', function (Blueprint $table) {
+
+            $table->increments('id');     
+            
+            $table->integer('id_user')->unsigned()->nullable();
+            $table->foreign('id_user')->references('id')->on('users');
+            
+            $table->text('msg')->nullable();
+            
+            $table->enum('status', array('A', 'I'))->nullable();                        
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('email_campanhas');
+    }
+
+}
---
+++ database/migrations/2019_01_01_000005_create_grupos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGruposTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        
+        Schema::create('grupos', function (Blueprint $table) {
+            $table->increments('id'); 
+            $table->text('name',50)->nullable();            
+            $table->enum('status', array('A', 'I'))->nullable();                        
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('grupos');
+    }
+
+}
